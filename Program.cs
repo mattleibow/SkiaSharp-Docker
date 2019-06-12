cf08b067da301359e13ebd382d765d13f4b3cf99
Matthew Leibowitz <mattleibow@live.com>
2019-06-12 17:20:24 +0200

    Updates

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using SkiaSharp;
 
 namespace docker
@@ -7,7 +10,20 @@ namespace docker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World == " + SKImageInfo.PlatformColorType);
+            Console.WriteLine($"Starting the app...");
+
+            var os = RuntimeInformation.OSDescription;
+            Console.WriteLine($"Your operating system is {os}.");
+
+            var assembly = typeof(SKImageInfo).Assembly;
+            var attributes = assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute));
+            var versionAttr = (AssemblyInformationalVersionAttribute)attributes.FirstOrDefault();
+            Console.WriteLine($"You are using SkiaSharp version {versionAttr?.InformationalVersion}.");
+
+            var colorType = SKImageInfo.PlatformColorType;
+            Console.WriteLine($"The color type for your platform is {colorType}.");
+
+            Console.WriteLine($"All done.");
         }
     }
 }
