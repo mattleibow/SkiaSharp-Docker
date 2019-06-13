f1376fd0a1c2407c2fae7beec696254ad146e992
Matthew Leibowitz <mattleibow@live.com>
2019-06-13 15:28:11 +0200

    Run locally as well

--- build.ps1
+++ build.ps1
@@ -1,5 +1,15 @@
+echo "Starting build..."
+
 dotnet restore
 dotnet publish -c Release -r debian-x64 -o bld
 
+if ($IsLinux) {
+    echo "Running locally..."
+    ./bld/Hello
+}
+
+echo "Running on Docker..."
 docker build -t dotnetapp-dev .
 docker run --rm dotnetapp-dev
+
+echo "All done."
